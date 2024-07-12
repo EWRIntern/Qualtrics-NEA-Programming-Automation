63e0847001f828ba8557edc6a9bd9237b53c724b
EWRIntern <raghavi@ewhiteresearch.com>
2024-07-12 09:53:26 -0400

    QualtricsProgrammingAutomationEWR.py

--- QualtricsProgrammingAutomationEWR.py
+++ QualtricsProgrammingAutomationEWR.py
@@ -5,6 +5,9 @@ import string
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -182,7 +185,12 @@ def import_survey(driver, survey_text_file):
     print("Survey imported successfully")
 
 def automate_survey_creation(doc_path, username, password, survey_name):
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
     try:
         login_to_qualtrics(driver, username, password)
         create_survey(driver, survey_name)
